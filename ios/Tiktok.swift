d0f31989c292cc26412e23353f72b80bfe4af5c6
James Dumay <james@hashgifted.com>
2022-02-27 16:55:45 +1100

    Make this work

--- ios/Tiktok.swift
+++ ios/Tiktok.swift
@@ -4,23 +4,23 @@ import Photos
 
 @objc(Tiktok)
 class Tiktok: UIViewController {
-    
+
   @objc
   func auth(_ callback: @escaping RCTResponseSenderBlock) {
-    let scopes = ["user.info.basic,video.list"] // list your scopes
+    let scopes = ["user.info.basic,video.list"]
     let scopesSet = NSOrderedSet(array:scopes)
     let request = TikTokOpenSDKAuthRequest()
     request.permissions = scopesSet
-
     DispatchQueue.main.async {
-      request.send(self, completion: { resp -> Void in
+        let topViewController = UIApplication.shared.keyWindow?.rootViewController;
+        request.send((topViewController)!, completion: { resp -> Void in
         callback([
           ["status": resp.errCode.rawValue, "code": resp.code]
         ])
       })
     }
   }
-  
+
   @objc
   func share(_ path: String, callback: @escaping RCTResponseSenderBlock) {
     PHPhotoLibrary.shared().performChanges({
