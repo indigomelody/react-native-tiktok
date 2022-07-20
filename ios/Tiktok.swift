2d7672e0a3617c0d5bccea62191c38ebc9a1fa6c
James Dumay <james@hashgifted.com>
2022-07-21 01:07:20 +1000

    Mutilate this to avoid redefinition error

--- ios/Tiktok.swift
+++ ios/Tiktok.swift
@@ -23,19 +23,6 @@ class Tiktok: UIViewController {
 
   @objc
   func share(_ path: String, callback: @escaping RCTResponseSenderBlock) {
-    PHPhotoLibrary.shared().performChanges({
-      let asset = PHAssetCreationRequest.forAsset()
-      asset.addResource(with: .video, fileURL: URL(string: path)!, options: nil)
-      let newImageIdentifier = asset.placeholderForCreatedAsset?.localIdentifier
 
-      let request = TikTokOpenSDKShareRequest()
-      request.mediaType = TikTokOpenSDKShareMediaType.video;
-      request.localIdentifiers = [newImageIdentifier!]
-      DispatchQueue.main.async {
-        request.send(completionBlock: { resp -> Void in
-          callback([resp.errCode.rawValue])
-        })
-      }
-    })
   }
 }
